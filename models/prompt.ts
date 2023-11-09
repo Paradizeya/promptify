da1c59fcc1f2f5adb4139375d365166eb667ef78
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-09 22:25:45 +0300

    created prompt model for MongoDB

---
+++ models/prompt.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, model, models } from "mongoose";
+import type { Model } from "mongoose";
+
+interface IPrompt {
+  creator: mongoose.Schema.Types.ObjectId;
+  prompt: string;
+  tag: string;
+}
+
+const PromptSchema = new Schema<IPrompt>({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required"],
+  },
+});
+
+const Prompt: Model<IPrompt> =
+  (models.Prompt as Model<IPrompt>) || model<IPrompt>("Prompt", PromptSchema);
+
+export default Prompt;
