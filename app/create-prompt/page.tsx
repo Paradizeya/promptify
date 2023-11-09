f946babfea99b83e5596d14afe6dea43a8c64b78
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-09 21:38:11 +0300

    added functionality

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,6 +1,38 @@
+"use client";
+import { FormEvent, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import Form from "@/components/form/Form";
 
 const CreatePage = () => {
+  const session = useSession();
+  const router = useRouter();
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e: FormEvent) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    try {
+      const response = await fetch("api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.data?.user.id,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <section className="create">
       <div>
@@ -10,7 +42,13 @@ const CreatePage = () => {
           imagination run wild with any AI-powered platform
         </p>
       </div>
-      <Form />
+      <Form
+        type={"Create"}
+        post={post}
+        setPost={setPost}
+        isSubmitting={isSubmitting}
+        handleSubmit={createPrompt}
+      />
     </section>
   );
 };
