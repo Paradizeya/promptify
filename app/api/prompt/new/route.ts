f5794d1de50821585764c7064d922fe20d2dee56
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-09 22:25:27 +0300

    added prompt/new api endpoint

---
+++ app/api/prompt/new/route.ts
@@ -0,0 +1,30 @@
+import { connectToDB } from "@/utils/database";
+import Prompt from "@/models/prompt";
+import { NextApiRequest } from "next";
+
+type DataType = {
+  userId: string;
+  prompt: string;
+  tag: string;
+};
+
+type CustomRequest = NextApiRequest & {
+  json: () => Promise<DataType>;
+};
+
+export const POST = async (req: CustomRequest) => {
+  const { userId, prompt, tag } = await req.json();
+
+  try {
+    await connectToDB();
+    const newPrompt = new Prompt({ creator: userId, prompt, tag });
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify("Failed to create a prompt"), {
+      status: 500,
+    });
+  }
+};
