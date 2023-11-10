d3e7a78bab52b15d3d0049996d733b6be847829c
Anastasia <56909283+Paradizeya@users.noreply.github.com>
2023-11-11 01:20:43 +0300

    Update Feed.tsx

--- components/feed/Feed.tsx
+++ components/feed/Feed.tsx
@@ -44,6 +44,9 @@ const Feed = () => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
+      if(!response.ok){
+        data = [];
+      }
       setPosts(data);
     };
     fetchPosts();
