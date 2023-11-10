945f234661902a8b49668273c7d27473d4965638
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-10 20:12:01 +0300

    api endpoint for posts fetching

---
+++ app/api/prompt/route.ts
@@ -0,0 +1,19 @@
+import { connectToDB } from "@/utils/database";
+import Prompt from "@/models/prompt";
+
+export const GET = async (req: any) => {
+  try {
+    await connectToDB();
+
+    const prompts = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return new Response(JSON.stringify("Failed to fetch all prompts"), {
+      status: 500,
+    });
+  }
+};
