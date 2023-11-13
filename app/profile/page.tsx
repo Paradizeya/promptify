2787781529611fe17ad430f8a4e9a63b62cc0366
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-13 22:31:15 +0300

    redirect from /profile to /profile/id

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,35 +1,17 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useSession } from "next-auth/react";
-import type { Post } from "@/components/feed/Feed";
-
-import PromptCardList from "@/components/promptCard/PromptCardList";
+import { useRouter } from "next/navigation";
 
 const ProfilePage = () => {
+  const router = useRouter();
   const session = useSession();
-  const [posts, setPosts] = useState<Post[]>([]);
-
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(`/api/user/${session?.data?.user.id}/posts`);
-      const data = await response.json();
-      if (response.ok) {
-        setPosts(data);
-      }
-    };
-    if (session.data?.user.id) fetchPosts();
-  }, [session?.data?.user.id]);
+    router.push(`/profile/${session?.data?.user.id}`);
+  }, [session.data?.user.id]);
 
-  const handleEdit = () => {};
-  const handleDelete = () => {};
-
-  return (
-    <section>
-      <h1>Profile of {session.data?.user.name}</h1>
-      <PromptCardList data={posts} />
-    </section>
-  );
+  return <></>;
 };
 
 export default ProfilePage;
