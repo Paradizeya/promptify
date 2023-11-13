a740edc67acb2f8a6acba5dde7bc2d0f8e4b729f
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-13 20:40:00 +0300

    display action buttons only at user profile

--- components/promptCard/PromptCard.tsx
+++ components/promptCard/PromptCard.tsx
@@ -3,16 +3,24 @@
 import Image from "next/image";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
 import type { Post } from "@/components/feed/Feed";
 type Props = {
   post: Post;
-  handleTagClick: () => void;
+  handleTagClick?: (tag: string) => void;
 };
 
 const PromptCard = ({ post, handleTagClick }: Props) => {
   const [copied, setCopied] = useState("");
   const session = useSession();
+  const pathName = usePathname();
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  };
 
   return (
     <article className="promptCard">
@@ -45,29 +53,36 @@ const PromptCard = ({ post, handleTagClick }: Props) => {
             alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={20}
             height={20}
+            onClick={() => handleCopy()}
           />
-          {session.data?.user.id === post.creator._id && (
-            <>
-              <Image
-                src={"/assets/icons/edit.svg"}
-                alt="edit_icon"
-                width={20}
-                height={20}
-              />
-              <Image
-                src={"/assets/icons/delete.svg"}
-                alt="delete_icon"
-                width={22}
-                height={22}
-              />
-            </>
-          )}
+          {session.data?.user.id === post.creator._id &&
+            pathName === "/profile" && (
+              <>
+                <Image
+                  src={"/assets/icons/edit.svg"}
+                  alt="edit_icon"
+                  width={20}
+                  height={20}
+                />
+                <Image
+                  src={"/assets/icons/delete.svg"}
+                  alt="delete_icon"
+                  width={22}
+                  height={22}
+                />
+              </>
+            )}
         </div>
       </div>
 
       <div className="promptCard__body">
         <p className="promptCard__post">{post.prompt}</p>
-        <a className="promptCard__tag">{post.tag}</a>
+        <a
+          className="promptCard__tag"
+          onClick={() => handleTagClick && handleTagClick(post.tag)}
+        >
+          {post.tag}
+        </a>
       </div>
     </article>
   );
