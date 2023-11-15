139b582f830ac0a9eb93b8efebe46ed93c1539a4
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-15 20:08:30 +0300

    turned to SSR

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,35 +1,18 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import type { Post } from "@/components/feed/Feed";
-import { useSession } from "next-auth/react";
-
+import getUser from "@/app/services/getUser";
+import getUserPosts from "@/app/services/getUserPosts";
 import PromptCardList from "@/components/promptCard/PromptCardList";
 
-const ProfilePage = ({ params }: { params: { id: string } }) => {
-  const session = useSession();
-  const [posts, setPosts] = useState<Post[]>([]);
+const ProfilePage = async ({ params }: { params: { id: string } }) => {
+  const isMyProfile = false;
 
-  const isMyProfile =
-    session?.data?.user.id && session.data.user.id === params.id;
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(`/api/user/${params.id}/posts`);
-      const data = await response.json();
-      if (response.ok) {
-        setPosts(data);
-      }
-    };
-    if (params?.id) fetchPosts();
-  }, [params.id]);
-
-  const handleEdit = () => {};
-  const handleDelete = () => {};
+  const posts = await getUserPosts(params.id);
+  const user = await getUser(params.id);
 
   return (
-    <section>
-      <h1>{isMyProfile ? `My Profile` : `Profile of ${params.id}`}</h1>
+    <section className="profile">
+      <h1 className="profile__title">
+        {isMyProfile ? `My Profile` : `${user.username}'s Profile`}
+      </h1>
       <PromptCardList data={posts} />
     </section>
   );
