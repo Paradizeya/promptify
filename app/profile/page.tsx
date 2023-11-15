62fa541069f38fb39a7c61feec8d1644cead06d5
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-16 00:01:53 +0300

    fix for warning

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,9 +5,9 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const ProfilePage = () => {
-  const router = useRouter();
   const session = useSession();
   useEffect(() => {
+    const router = useRouter();
     router.replace(`/profile/${session?.data?.user.id}`);
   }, [session.data?.user.id]);
 
