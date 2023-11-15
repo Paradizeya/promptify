e1792d154844722a0e0a641a90c2dfc6f75db6e3
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-15 20:07:57 +0300

    turned to SSR

--- components/feed/Feed.tsx
+++ components/feed/Feed.tsx
@@ -1,53 +1,13 @@
-"use client";
-
-import { useState, useEffect } from "react";
 import PromptCardList from "@/components/promptCard/PromptCardList";
+import getPosts from "@/app/services/getPosts";
+import SearchForm from "@/components/feed/SearchForm";
 
-export type Post = {
-  _id: string;
-  userId: string;
-  prompt: string;
-  tag: string;
-  creator: {
-    _id: string;
-    username: string;
-    email: string;
-    image: string;
-  };
-};
-
-const Feed = () => {
-  const [SearchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState<Post[]>([]);
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setSearchText(e.target.value);
-  };
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("/api/prompt");
-      const data = await response.json();
-      if (response.ok) {
-        setPosts(data);
-      }
-    };
-    fetchPosts();
-  }, []);
-
+const Feed = async () => {
+  const posts = await getPosts();
   return (
     <section className="feed">
-      <form className="feed__form">
-        <input
-          className="feed__searchBar"
-          type="search"
-          placeholder="Search for a tag or a username"
-          value={SearchText}
-          onChange={handleSearchChange}
-          required
-        />
-      </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <SearchForm />
+      <PromptCardList data={posts} />
     </section>
   );
 };
