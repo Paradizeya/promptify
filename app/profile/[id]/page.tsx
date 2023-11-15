512a9af9131f4a5966e043158c2cd2f13573da7f
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-15 20:20:15 +0300

    check if My Profile

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,9 +1,12 @@
 import getUser from "@/app/services/getUser";
 import getUserPosts from "@/app/services/getUserPosts";
 import PromptCardList from "@/components/promptCard/PromptCardList";
+import { authConfig } from "@/app/configs/auth";
+import { getServerSession } from "next-auth/next";
 
 const ProfilePage = async ({ params }: { params: { id: string } }) => {
-  const isMyProfile = false;
+  const session = await getServerSession(authConfig);
+  const isMyProfile = session?.user.id === params.id ? true : false;
 
   const posts = await getUserPosts(params.id);
   const user = await getUser(params.id);
