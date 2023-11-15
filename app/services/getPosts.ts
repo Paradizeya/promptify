3d1badc044f40947a25c9317935bf49f40dc0fdf
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-15 21:04:36 +0300

    extended revalidation period

--- app/services/getPosts.ts
+++ app/services/getPosts.ts
@@ -2,7 +2,7 @@ import type Post from "@/types/Post";
 
 const getPosts = async (): Promise<Post[]> => {
   const response = await fetch(process.env.URL + "/api/prompt", {
-    next: { revalidate: 1000, tags: ["allPosts"] },
+    next: { revalidate: 10000, tags: ["allPosts"] },
   });
   const data = await response.json();
   if (response.ok) {
--- app/services/getUserPosts.ts
+++ app/services/getUserPosts.ts
@@ -2,7 +2,7 @@ import Post from "@/types/Post";
 
 const getUserPosts = async (id: string): Promise<Post[]> => {
   const response = await fetch(process.env.URL + `/api/user/${id}/posts`, {
-    next: { revalidate: 1, tags: [`${id}_posts`] },
+    next: { revalidate: 10000, tags: [`${id}_posts`] },
   });
   const data = await response.json();
   if (response.ok) {
