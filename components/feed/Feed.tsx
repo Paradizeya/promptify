b8e3895a0460444c693129a3b9baa5177660a736
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-11-22 03:04:11 +0300

    added search functionality

--- components/feed/Feed.tsx
+++ components/feed/Feed.tsx
@@ -1,13 +1,56 @@
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 import PromptCardList from "@/components/promptCard/PromptCardList";
 
 import SearchForm from "@/components/feed/SearchForm";
 import Post from "@/types/Post";
 
 const Feed = ({ posts }: { posts: Post[] | null }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search");
+  const tag = searchParams.get("tag");
+  const [displayPosts, setDisplayPosts] = useState(posts);
+
+  useEffect(() => {
+    //Filtering function
+    //Filter by tag if exists
+    //Else filter by search param if it exists
+    //Otherwise no filter => display original posts
+    if (posts !== null) {
+      if (tag !== null) {
+        const result = posts.filter((post) => {
+          return post.tag.includes(tag);
+        });
+        setDisplayPosts(result);
+      } else if (search !== null) {
+        const result = posts.filter((post) => {
+          const usernameMatch = post.creator.username
+            .toLocaleLowerCase()
+            .includes(search);
+          const tagMatch = post.tag.toLocaleLowerCase().includes(search);
+          const promptMatch = post.prompt.toLocaleLowerCase().includes(search);
+          return usernameMatch || tagMatch || promptMatch;
+        });
+        setDisplayPosts(result);
+      } else {
+        setDisplayPosts(posts);
+      }
+    }
+  }, [search, tag]);
+
+  const handleTagClick = (tag: string) => {
+    router.push(`?tag=${tag}`);
+  };
+
   return (
     <section className="feed">
       <SearchForm />
-      {posts && <PromptCardList data={posts} />}
+      {displayPosts !== null && (
+        <PromptCardList data={displayPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
--- components/feed/SearchForm.tsx
+++ components/feed/SearchForm.tsx
@@ -1,25 +1,44 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 type Props = {};
 
 const SearchForm = (props: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search");
+
   const [SearchText, setSearchText] = useState("");
+  const startSearch = () => {
+    router.push(`${SearchText === "" ? "/" : `?search=${SearchText}`}`);
+  };
+
+  useEffect(() => {
+    if (search !== null) {
+      setSearchText(search);
+    } else setSearchText("");
+  }, [search]);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    startSearch();
+  };
+
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    setSearchText(e.target.value);
+    setSearchText(e.target.value.toLocaleLowerCase());
   };
 
   return (
-    <form className="feed__form">
+    <form className="feed__form" onSubmit={(e) => handleSubmit(e)}>
       <input
         className="feed__searchBar"
         type="search"
-        placeholder="Search for a tag or a username"
+        placeholder="Search for a tag, username or keywords"
         value={SearchText}
         onChange={handleSearchChange}
-        required
       />
     </form>
   );
